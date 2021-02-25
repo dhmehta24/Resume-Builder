444eaf1392c70810525b7ae56bdc45fe6783ce5e
Shivanshu Shrivastava <ssshrivastava2@gmail.com>
2021-02-25 09:33:53 -0800

    Forms are modified

--- resume_maker/urls.py
+++ resume_maker/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('resume',views.render_page,name='res'),
-    path('get',views.get_input,name='input'),
-    #path('add/<slug:field>',views.add_field,name = "add_field")
+    path('site.html', views.render_page, name='res'),
+    path('getinput.html', views.get_input, name='input'),
+    path('Education.html', views.edu, name='Education'),
+    path('work.html', views.wor, name='work'),
+    path('position.html', views.pos, name='position'),
+    path('project.html', views.pro, name='project'),
+    path('academic.html', views.aca, name='academic'),
+    path('extra.html', views.ext, name='extra'),
+
+    # path('add/<slug:field>',views.add_field,name = "add_field")
 ]
